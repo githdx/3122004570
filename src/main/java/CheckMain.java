036307c488a963c73b145be1434744efdc672276
dong <3122004570@mail2.gdut.edu.cn>
2024-03-13 07:34:43 +0800

    相似度写入txt

--- src/main/java/CheckMain.java
+++ src/main/java/CheckMain.java
@@ -5,17 +5,20 @@ import java.util.List;
 
 public class CheckMain {
     public static void main(String[] args) {
+        //1.读取txt文件
         String txt1=TxtHandle.txtRead("../../src/resources/orig.txt");
         String txt2=TxtHandle.txtRead("../../src/resources/orig_0.8_add.txt");
 
-        //1.获取SimHash
+        //2.获取SimHash
         int[] weightVectorList1=Calculation.getSiHash(txt1);
         int[] weightVectorList2=Calculation.getSiHash(txt2);
 
-        //2.计算海明距离
+        //3.计算海明距离
         int distance=Calculation.calculateHammingDistance(weightVectorList1,weightVectorList2);
-        //3.计算论文相似度
+        //4.计算论文相似度
         double similarity=Calculation.calculateHammingSimilarity(distance);
         System.out.println("论文相似度："+similarity+"%");
+        //5.将结果写入文件
+        TxtHandle.txtWrite("../../src/resources/answer.txt",similarity);
     }
 }
