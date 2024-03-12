f4d9c3c87ac7d4695506387fdd2e8d0af59563e7
dong <3122004570@mail2.gdut.edu.cn>
2024-03-13 07:49:18 +0800

    打包成main-jar

--- src/main/java/CheckMain.java
+++ src/main/java/CheckMain.java
@@ -1,24 +1,19 @@
-import com.hankcs.hanlp.HanLP;
-
-import java.util.Arrays;
-import java.util.List;
-
 public class CheckMain {
     public static void main(String[] args) {
         //1.读取txt文件
-        String txt1=TxtHandle.txtRead("../../src/resources/orig.txt");
-        String txt2=TxtHandle.txtRead("../../src/resources/orig_0.8_add.txt");
-        if(txt1==null||txt2==null)return;
+        String originTxt= TxtHandle.txtRead(args[0]);
+        String copyTxt= TxtHandle.txtRead(args[1]);
+        if(originTxt==null||copyTxt==null)return;
         //2.获取SimHash
-        int[] weightVectorList1=Calculation.getSimHash(txt1);
-        int[] weightVectorList2=Calculation.getSimHash(txt2);
-        if(weightVectorList1==null||weightVectorList2==null)return;
+        int[] weightVectorListO=Calculation.getSimHash(originTxt);
+        int[] weightVectorListC=Calculation.getSimHash(copyTxt);
+        if(weightVectorListO==null||weightVectorListC==null)return;
         //3.计算海明距离
-        int distance=Calculation.calculateHammingDistance(weightVectorList1,weightVectorList2);
+        int distance=Calculation.calculateHammingDistance(weightVectorListO,weightVectorListC);
         //4.计算论文相似度
         double similarity=Calculation.calculateHammingSimilarity(distance);
         System.out.println("论文相似度："+similarity+"%");
         //5.将结果写入文件
-        TxtHandle.txtWrite("../../src/resources/answer.txt",similarity);
+        TxtHandle.txtWrite(args[2],similarity);
     }
 }
